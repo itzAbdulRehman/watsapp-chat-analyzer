a9ba4477acb73c2b20cf106b222cb1eccf400758
AbdulRehman <itzabdulrehman786@gmail.com>
2022-08-26 21:18:10 +0500

    make it better

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 
 
 
-st.sidebar.title("Watsapp Chat Analyzer")
+st.sidebar.title("Whatsapp Chat Analyzer")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
      # To read file as bytes:\
@@ -51,12 +51,12 @@ if uploaded_file is not None:
           st.title("Monthly Timeline")
           timeline = helper.get_month(seleted_user,df)
           fig = px.line(timeline, y='message', x='month',  width=785,height=600, template='plotly_dark', markers=True)
-          st.plotly_chart(fig)
+          st.plotly_chart(fig,use_container_width=True)
 
           st.title('Daily Timeline')
           dayline = helper.get_day(seleted_user,df)
           fig = px.line(dayline, y='message', x='date',  width=785,height=600, template='plotly_dark', markers=True)
-          st.plotly_chart(fig)
+          st.plotly_chart(fig,use_container_width=True)
 
 
           st.title("Activity Map")
@@ -64,12 +64,13 @@ if uploaded_file is not None:
           with col1:
                busyday = helper.most_busy_day(seleted_user, df)
                fig = px.bar(busyday, y='message', x='day', height=400,width=420, template='plotly_dark')
-               st.plotly_chart(fig)
+               st.plotly_chart(fig,use_container_width=True)
           with col2:
                busymonth = helper.most_busy_week(seleted_user,df)
                fig = px.bar(busymonth, y='message', x='month', height=400,width=420, template='plotly_dark')
-               st.plotly_chart(fig)
+               st.plotly_chart(fig,use_container_width=True)
 
+          st.title('Weekly Activity Map')
           pivt = helper.weekly_activity_map(seleted_user,df)
           fig,ax = plt.subplots()
           sns.set(rc={'figure.facecolor': '7F5283'})
@@ -87,7 +88,7 @@ if uploaded_file is not None:
                with col2:
                     st.header('Most Busy Users')
                     fig = px.bar(most_busy_user, x='Name', y='Messages', template='plotly_dark', height=400,width=420)
-                    st.plotly_chart(fig)
+                    st.plotly_chart(fig,use_container_width=True)
 
 
 
@@ -101,7 +102,7 @@ if uploaded_file is not None:
           st.title('Most Repeated Words')
           most_repeted_words = helper.most_repeated_words(seleted_user,df)
           fig = px.bar(most_repeted_words, y='message', x='count', width=785,height=600, template='plotly_dark')
-          st.plotly_chart(fig)
+          st.plotly_chart(fig,use_container_width=True)
 
           emoji = helper.emoji_use(seleted_user,df)
 
@@ -115,12 +116,12 @@ if uploaded_file is not None:
                fig, ax = plt.subplots()
                emoji_head=emoji.head(5)
                fig = px.pie(emoji_head, values='counter', names='emoji',color_discrete_sequence=px.colors.sequential.Teal_r,height=409,width=500)
-               st.plotly_chart(fig)
+               st.plotly_chart(fig,use_container_width=True)
 
 
 else:
      st.subheader('Instructions:')
-     st.caption("Export your chat from Watsapp")
+     st.caption("Export your chat from Whatsapp")
      st.caption("Your Exported chat must be WITHOUT MEDIA")
      st.caption('Download or save your chat file and upload here to see the Magic!')
      st.image("https://fs.npstatic.com/userfiles/7715851/image/NextPit-Export-WhatsApp-Chat-1-w782.png")
